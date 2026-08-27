27c0694b57fdb08691054da92248e930701624b2
Marcos Vinnicius <marconvinnicius@gmail.com>
2026-08-27 16:09:45 -0300

    feat: cria a versão inicial com o sorteio de palavras

---
+++ index.js
@@ -0,0 +1,41 @@
+const readline = require ('readline/promises');
+const {stdin: input, stdout: output} = require ('process');
+
+const rl = readline.createInterface({ input, output });
+
+async function  iniciarJogo(){
+    const palavras = ["BACKEND", "NODEJS", "JAVASCRIPT", "EXPRESS", "SERVIDOR", "TERMINAL"];
+
+    const indiceAleatorio = Math.floor(Math.random() * palavras.length);
+    const palavraSecreta = palavras[indiceAleatorio];
+
+    let letrasDescobertas = Array(palavraSecreta.length).fill("_");
+    let jogoRodando = true;
+    
+    console.log("=== Bem-Vindo ao Jogo da Forca ===");
+
+    while (jogoRodando){
+        console.log(`\nPalavra atual: ${letrasDescobertas.join(" ")}`)
+        const chute = (await rl.question("Digite uma letra: ")).toUpperCase();
+        let acertou = false;
+
+        for(let i=0; i<palavraSecreta.length; i++) {
+            if (palavraSecreta[i] === chute) {
+                letrasDescobertas[i] = chute
+                acertou = true;
+            }
+        }
+
+        if(!acertou){
+            console.log("[X] Letra Incorreta!");
+        }
+        if(!letrasDescobertas.includes("_")){
+            console.log(`\n[VITORIA] Parabens! Você descobriu a palavra: ${palavraSecreta}`);
+            jogoRodando = false;
+        }
+    }
+
+    rl.close();
+}
+
+iniciarJogo();
